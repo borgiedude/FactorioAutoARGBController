9b48c04e72ae8609fa939adc8c2805b9a2998644
Adrian Christopher Borg <a.borg@federation.edu.au>
2024-10-28 15:44:08 +1100

    Added keyboard control, ctrl+shift+r to find the x, then zoom to exactly the x position in future searches, removing the need to init factorio

--- FactorioARGBControl.py
+++ FactorioARGBControl.py
@@ -1,12 +1,14 @@
+from re import search
 import win32gui
 import win32con
 import win32api
 import time
 import pyautogui
 import pygame
+import keyboard
+from PIL import Image
 
 xSearch = 'XSmall.png'
-bottomBar = 'BottomBar.png'
 threat_button = 'Breathing.png'
 no_threat_button = 'Spectrum.png'
 confidence_level = 0.7  # 70% match confidence
@@ -16,8 +18,6 @@ threat_click_position = None
 hwnd = None
 
 def initMasterPlus():
-    # Initialize the mixer module
-    pygame.mixer.init()
     # Load the audio file
     pygame.mixer.music.load("bell.mp3")
 
@@ -43,36 +43,6 @@ def initMasterPlus():
     
     pygame.mixer.music.play()
 
-def initFactorio():
-    global search_region
-    while not search_region:
-        try:
-            # Search for the image within the specified region with confidence
-            search_region = pyautogui.locateOnScreen(bottomBar, confidence=0.9)
-
-            if search_region:
-                left = int(search_region.left)
-                top = int(search_region.top)
-                pygame.mixer.music.play()
-                # print(f"Toolbar found at: {search_region}")
-                # print(f"Left {left}")
-                # print(f"Top {top}")
-                # Optional: Move the mouse to the center of the found image
-                #pyautogui.moveTo(pyautogui.center(location))
-                
-        except pyautogui.ImageNotFoundException:
-            pass
-            #print("ImageNotFoundException: The specified image could not be found.") 
-        
-        time.sleep(1)
-
-    top = top - 100
-    width = 150
-    height = 150
-    search_region = (left, top, width, height)
-    pygame.mixer.music.load("trumpet.mp3")
-    pygame.mixer.music.play()
-
 def get_window_handle(window_title):
     hwnd = win32gui.FindWindow(None, window_title)
     if hwnd == 0:
@@ -82,10 +52,13 @@ def get_window_handle(window_title):
 def click_on_background_window(hwnd, pos):
     # Post a click message to the window without bringing it to the front
     # print("pos[0]" + str(int(pos[0])) + " pos[1]:" + str(int(pos[1])))
-    lParam = win32api.MAKELONG(pos[0], pos[1])
-    #win32gui.SetForegroundWindow(hwnd)
-    win32gui.PostMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
-    win32gui.PostMessage(hwnd, win32con.WM_LBUTTONUP, 0, lParam)
+    try:
+        lParam = win32api.MAKELONG(pos[0], pos[1])
+        #win32gui.SetForegroundWindow(hwnd)
+        win32gui.PostMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
+        win32gui.PostMessage(hwnd, win32con.WM_LBUTTONUP, 0, lParam)
+    except TypeError:
+        print("Coolermaster Window Not Found")
 
 def monitorFactorio():
     dangerMode = False
@@ -95,20 +68,17 @@ def monitorFactorio():
         try:
             # Search for the image on the screen
             spot = pyautogui.locateOnScreen(xSearch, region=search_region, confidence=confidence_level) #region=search_region, 
-
+            print("Threat Detected")
             noThreatTime = -1
             if not dangerMode:
                 #detectTime = time.time()
                 dangerMode = True
-                # print("Dangermode True")
+                #print("Danger Detected")
                 click_on_background_window(hwnd, threat_click_position)  # Replace with your desired coordinates
                 pass
-                # print("Clicking on danger position")
-            #print(f"Image found at: {location}")
-            # Optional: Move the mouse to the center of the found image
-            #pyautogui.moveTo(pyautogui.center(location))
                 
         except pyautogui.ImageNotFoundException:
+            print("No Threat Detected")
             # print("Threat Image not found")
             if noThreatTime == -1:
                 noThreatTime = time.time()
@@ -123,7 +93,39 @@ def monitorFactorio():
 
         time.sleep(0.3)
 
+def update_danger_position():
+    global search_region, confidence_level
+    search_region = None
+    while not search_region:
+        try:
+            search_region = pyautogui.locateOnScreen(xSearch, confidence=confidence_level)
+
+            if search_region:
+                left = int(search_region.left)
+                top = int(search_region.top)
+                #pygame.mixer.music.play()
+                
+        except pyautogui.ImageNotFoundException:
+            pass
+            #print("ImageNotFoundException: The specified image could not be found.") 
+        
+        time.sleep(1)
+
+    with Image.open(xSearch) as img:
+        width, height = img.size
+
+    search_region = (left, top, width, height)
+    pygame.mixer.music.load("trumpet.mp3")
+    pygame.mixer.music.play()
+
+def detect_key_combination():
+    print("Listening for Control + Shift + R...")
+    # Detects when the keys Control + Shift + R are pressed together
+    keyboard.add_hotkey('ctrl+shift+r', update_danger_position)
+
 if __name__ == "__main__":
-    initMasterPlus()
-    initFactorio()
-    monitorFactorio()
+    pygame.mixer.init()         # Initialize the mixer module
+    detect_key_combination()    # Start listening for ctrl+shift+r to reassign the x position
+    #initMasterPlus()           # Find and ready the coolermaster software for control
+    # update_danger_position()  # Look for the X for the first time, could do this by default, but could also only do it on command to be more performant.
+    monitorFactorio()           # begin the main monitoring loop
